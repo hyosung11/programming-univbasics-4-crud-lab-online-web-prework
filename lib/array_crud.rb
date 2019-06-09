9c0a269f868602479b66622419f9e5e4d230cc7e
HyoSung Bidol-Lee <hyosung11@me.com>
2019-06-09 18:17:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array << "arrays!"
 end
 
-def add_element_to_start_of_array(array)
+def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array = ["I", "am", "really", "learning"]
   add_element_to_start_of_array.unshift("wow")
 end
