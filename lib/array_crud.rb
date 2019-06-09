4dc0a9325eeb49f623a188645ff978ab796eaaff
HyoSung Bidol-Lee <hyosung11@me.com>
2019-06-09 18:01:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,6 @@ end
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array =  ["I", "am", "really", "learning", "arrays!"]
   remove_element_from_end_of_array.pop("arrays!")
-  
 end
 
 def remove_element_from_start_of_array(array)
